6990c3dafb0b64cdad552b0cb72a4483fe4ebc66
Sumitsingh39499 <sumitsingh22012005@gmail.com>
2026-04-27 14:17:33 +0530

    Update API URLs for production

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -3,7 +3,7 @@ import { store } from "../store/store.js"
 import { setCredentials, logout } from "../store/authSlice.js"
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
     withCredentials: true
 });
 
